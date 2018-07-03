cce6d478b81ff9839541ad5ffdb5db7eb42c9c3d
kompel <ziombartekyo@gmail.com>
2018-07-03 16:38:13 +0200

    Icannoteven

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("This site is heavily unfinished, literally nothing is here. You have been warned")
+//alert("This site is heavily unfinished, literally nothing is here. You have been warned")
 
 //Variables
 var creativity = 0;
